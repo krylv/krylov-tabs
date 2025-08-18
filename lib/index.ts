d4315f4a968b01eb6816eb77d6eecd39bfd09c5f
krylov42 <worldbalance1337@gmail.com>
2025-08-18 10:46:37 +0500

    master feat: add global imports for variants

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { Tags, Tag } from "./components";
 export type { TagProps, TagsProps } from "./components";
 export { RowTags, SwiperTags, TooltipTags } from "./variants";
+export type { IRowTags, ISwiperTags, ITooltipTags } from "./variants";
--- lib/variants/index.ts
+++ lib/variants/index.ts
@@ -1,3 +1,6 @@
 export { RowTags } from "./RowTags";
 export { SwiperTags } from "./SwiperTags";
 export { TooltipTags } from "./TooltipTags";
+export type { IRowTags } from "./RowTags";
+export type { ISwiperTags } from "./SwiperTags";
+export type { ITooltipTags } from "./TooltipTags";
